30bb93d729b1622636acb5d20f95b08f794ac5b3
newyorkannie <newyorkannie7@gmail.com>
2024-04-10 05:28:35 -0700

    Add files via upload

--- api-test.js
+++ api-test.js
@@ -1,15 +1,23 @@
 const request = require('request');
 
+//the local interpreter is index.js file
+const { Interpreter, OPCODE_MAP } = require('./interpreter/index_7');
+
+//1 of these 2 lines
+const { STOP, ADD, PUSH, STORE, LOAD } = OPCODE_MAP || {};
+//const { STOP, ADD, PUSH, STORE, LOAD } = OPCODE_MAP;
+
+
 const BASE_URL = 'http://localhost:3000';
 
-const postTransact = ({ to, value }) => {
+const postTransact = ({ code, to, value, gasLimit }) => {
     return new Promise((resolve, reject) => {
         //in this callback area, we will trigger
         //which is this endpoint
         request(`${BASE_URL}/account/transact`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ to, value })
+            body: JSON.stringify({ code, to, value, gasLimit })
         },(error, reponse, body) => {
             //the response is going to come back as a string
             //we can resolve this as part of the promise 
@@ -24,9 +32,22 @@ const getMine = () => {
             request(`${BASE_URL}/blockchain/mine`, (error, response, body) => {
                 return resolve(JSON.parse(body));
             });
-        }, 1000);
+        }, 3000);
     });
 }
+
+const getAccountBalance = ({ address } = {}) => {
+    return new Promise((resolve, reject) => {
+        request(
+        `${BASE_URL}/account/balance` + (address ? `?address=${address}`: ''), 
+        (error, response, body) => {
+            return resolve(JSON.parse(body));
+        });
+    });
+}
+
+let toAccountData;
+let smartContractAccountData;
 
 postTransact({})
     .then(postTransactResponse => {
@@ -35,22 +56,65 @@ postTransact({})
             postTransactResponse
         );
 
-        const toAccountData = postTransactResponse.transaction.data.accountData;
+        toAccountData = postTransactResponse.transaction.data.accountData;
 
         return getMine();
-        //replace 'foo-recipient' with 'toAccountData.address'
-        //postTransact({ to: 'foo-recipient', value: 20 })
-        //postTransact({ to: toAccountData.address, value: 20 })
     }).then(getMineResponse => {
         console.log('getMineResponse', getMineResponse);
-
-        //test call postTransact with receipient and value fields
-        return postTransact({ to: 'foo-recipient', value: 20 })
+        
+        return postTransact({ to: toAccountData.address, value: 20 });
     })
-
     .then(postTransactResponse2 => {
         console.log(
-        'postTransactResponse (Standard Transaction)',
+        'postTransactResponse2 (Standard Transaction)',
         postTransactResponse2
     );
+
+    const key = 'foo';
+    const value = 'bar';
+    const code = [PUSH, value, PUSH, key, STORE, PUSH, key, LOAD, STOP];
+    
+    return postTransact({ code });
+    })
+    .then(postTransactResponse3 => {
+        console.log(
+            'postTransactResponse3 (Smart Contract)',
+            postTransactResponse3
+        );
+
+        smartContractAccountData = postTransactResponse3
+        .transaction
+        .data
+        .accountData;
+
+    return getMine();
+})           
+.then(getMineResponse2 => {
+    console.log('getMineResponse2', getMineResponse2);
+
+    return postTransact({ 
+        to: smartContractAccountData.codeHash,
+        value: 0,
+        gasLimit: 100
+    });
 })
+.then(postTransactResponse4 => {
+    console.log(
+        'postTransactResponse4 (to the Smart Contract)',
+        postTransactResponse4
+    );
+    return getMine();
+})           
+.then(getMineResponse3 => {
+    console.log('getMineResponse3', getMineResponse3);
+
+    return getAccountBalance();
+})
+.then(getAccountBalanceResponse => {
+    console.log('getAccountBalanceResponse', getAccountBalanceResponse );
+
+    return getAccountBalance({ address: toAccountData.address });
+})
+.then(getAccountBalanceResponse2 => {
+    console.log('getAccountBalanceResponse2', getAccountBalanceResponse2);
+});
